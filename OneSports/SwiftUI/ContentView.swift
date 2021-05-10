870fb192a3e4d22ccca05f11921f88d1c0118bc3
raghuveerchikilli5 <raghuveerchikilli@sreenidhi1sports.com>
2021-05-10 11:16:37 +0530

    updated code

--- OneSports/SwiftUI/ContentView.swift
+++ OneSports/SwiftUI/ContentView.swift
@@ -9,50 +9,27 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-       
-        VStack { // << moved this up to ZStack
-            
             ZStack {
-
+                VStack{
                Image("Background Red Colour")
                    .resizable()
-                .scaledToFit().padding(.top)
-    
-                Image("SreeNidhi Logo")
-                    .resizable()
-                    .scaledToFit().padding(.top).position(x:50, y: 145).frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: .center)
+                .scaledToFit().padding(.bottom)
+                Spacer()
                 
-    
+                Image("")
+                  
+                
+                }
 
 }
             
-          
-            
-                    Text("Top Text")
-                        .fontWeight(.light)
-                        .multilineTextAlignment(.center)
-                        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 100)
-                        .font(.body)
-            
-            
-                    Text("Top Teyutxt")
-                        .fontWeight(.light)
-                        .multilineTextAlignment(.leading)
-                        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 100)
-                        .font(.body)
-            
-            
-                }
-      
-        Image("Background Red Colour")
-            .resizable()
-         .scaledToFit().padding(.top)
-            
         
-    }}
-
+            
+    
+    }
+}
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView().body.position(x:160, y: 200.0)
+        ContentView()
     }
 }
