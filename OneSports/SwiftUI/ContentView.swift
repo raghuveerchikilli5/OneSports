6abb13a198305a2ca76802ba8e9b1a965327eb4e
raghuveerchikilli5 <raghuveerchikilli@sreenidhi1sports.com>
2021-05-11 18:43:43 +0530

    created login screen and  added   login API and fetch details API

--- OneSports/SwiftUI/ContentView.swift
+++ OneSports/SwiftUI/ContentView.swift
@@ -1,31 +1,126 @@
-//
-//  ContentView.swift
-//  OneSports
-//
-//  Created by Apple on 04/05/21.
-//
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        NavigationView {
             ZStack {
+                VStack {
                 VStack{
-               Image("Background Red Colour")
-                   .resizable()
-                .scaledToFit().padding(.bottom)
-                Spacer()
-                
-                Image("")
-                  
-                
+                               Image("Background Red Colour")
+                                   .resizable()
+                                   .scaledToFill().padding(.top)
+                                .frame(maxWidth: .infinity)
+                                .frame(maxHeight:250)
+                         
+                            .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                   
                 }
-
-}
-            
-        
-            
-    
+                VStack {
+                    VStack(alignment: .center) {
+                                  Image("logo_1")
+                                    .renderingMode(.original)
+                                    .resizable()
+                                    .frame(width: 260, height: 200, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                                    .scaledToFit()
+                                   
+                                    .padding(.top, -115)
+                       
+                    }
+                 VStack(spacing: 30) {
+                           
+                        Text("Discover best sports academy from over 1000 sports academy ")
+                            .frame(width: 300, height: 50, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                            .multilineTextAlignment(.center)
+                    
+                   
+                    NavigationLink(destination: login()) {
+                                              Text("Login")
+                                                  .frame(width: 300, height: 30, alignment: .center)
+                                                  .padding()
+                                                  .foregroundColor(.white)
+                                                  .background(Color.red)
+                                                  .cornerRadius(35)
+                                              
+                                              
+                    }.padding(.bottom,1)
+                                          
+                    
+                    Button(action: {
+                           print("sign up bin tapped")
+                       }) {
+                           Text("Create an Account")
+                            .frame(width: 300, height: 30, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                               .font(.system(size: 18))
+                            .padding()
+                               .foregroundColor(.red)
+                               .overlay(
+                                   RoundedRectangle(cornerRadius: 35)
+                                       .stroke(Color.red, lineWidth: 2)
+                           )
+                    }.padding(.top,-10)
+                    
+                    Text("Or login with")
+                        .frame(width: 300, height: 50, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                        .multilineTextAlignment(.center)
+                        .padding(.top,-20)
+                    HStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, spacing: 30) {
+                        Button(action: {
+                               print("sign up bin tapped")
+                           }) {
+                              
+                               
+                            Image("facebook")
+                                .resizable()
+                            .frame(width: 70, height: 70, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                                   
+                                   
+                                   .overlay(
+                                       RoundedRectangle(cornerRadius: 35)
+                                           .stroke(Color.blue, lineWidth: 2)
+                               )
+                           }
+                        Button(action: {
+                               print("sign up bin tapped")
+                           }) {
+                              
+                               
+                            Image("apple")
+                                .resizable()
+                            .frame(width: 70, height: 70, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                                   
+                                   
+                                   .overlay(
+                                       RoundedRectangle(cornerRadius: 35)
+                                           .stroke(Color.black, lineWidth: 2)
+                               )
+                           }
+                        
+                        Button(action: {
+                               print("sign up bin tapped")
+                           }) {
+                              
+                               
+                            Image("Google")
+                                .resizable()
+                            .frame(width: 70, height: 70, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                                   
+                                   
+                                   .overlay(
+                                       RoundedRectangle(cornerRadius: 35)
+                                           .stroke(Color.red, lineWidth: 2)
+                               )
+                           }
+                       
+                    }.padding(.bottom,80)
+                    
+                
+               Spacer()
+                }
+                  Spacer()
+                }
+    }
+            }
+        }
     }
 }
 struct ContentView_Previews: PreviewProvider {
---
+++ OneSports/SwiftUI/login.swift
@@ -0,0 +1,202 @@
+//
+//  login.swift
+//  OneSports
+//
+//  Created by apple on 11/05/21.
+//
+
+import SwiftUI
+import Alamofire
+
+
+struct login: View {
+    @State var mail: String = ""
+    @State var password: String = ""
+    @State var login1:Bool = false
+    @State var tokenMain = ""
+    var body: some View {
+        NavigationView {
+            ZStack {
+            VStack(spacing: 10) {
+                Text("Login")
+                    .foregroundColor(Color.black)
+                    .multilineTextAlignment(.center)
+                    .padding(.top, 0)
+                    .font(.system(size: 22))
+                Text("Add your details to login")
+                    .foregroundColor(Color.black)
+                    .multilineTextAlignment(.center)
+                    .padding(.top, 0)
+                    .font(.system(size: 12))
+                
+                TextField("   Your Mail", text: $mail)
+                .frame(width: 300, height: 50, alignment: .center)
+                    
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 35)
+                            .stroke(Color.gray, lineWidth: 2)
+                    )
+                    .padding(.top,20)
+                
+                TextField("    Password", text: $password)
+                .frame(width: 300, height: 50, alignment: .center)
+                    
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 35)
+                            .stroke(Color.gray, lineWidth: 2)
+                    )
+                    .padding(.top,20)
+                
+                Button(action: {
+                        print("SwiftUI: Button tapped")
+                        // Call func in SomeView()
+                    primaryAPI()
+                      
+                })
+                {
+                        Text("Login")
+                        .frame(width: 270, height: 20, alignment: .center)
+                        .padding()
+                        .foregroundColor(.white)
+                        .background(Color.red)
+                        .cornerRadius(35)
+                    
+                    
+                }
+                .padding()
+                
+                
+                
+                NavigationLink(destination: login()) {
+                    Text("forgot Your Passowrd?")
+                        .frame(width: 270, height: 20, alignment: .center)
+                        .padding(.top,0)
+                        
+                        .foregroundColor(.black)
+                        
+                        .cornerRadius(35)
+                        .font(.system(size: 14))
+                    
+                }
+                Spacer()
+                HStack {
+                    
+                    Text("dont Have An Account?")
+                        .frame(width: 152, height: 20, alignment: .center)
+                       
+                        
+                        .foregroundColor(.black)
+                        
+                        .cornerRadius(35)
+                        .font(.system(size: 14))
+                    
+                    NavigationLink(destination: login()) {
+                        Text("Signup")
+                            
+                            
+                            .padding(.leading,0)
+                            .foregroundColor(.blue)
+                            
+                            .cornerRadius(35)
+                            .font(.system(size: 14))
+                            .frame(width: 50, height: 100, alignment: .center)
+                    }
+                   
+                    
+                    Spacer()
+                    
+                } .frame(width: 220, height: 50, alignment: .center)
+                
+               
+            }
+               
+            .padding(.top,60)
+                
+            
+            
+    }.navigationBarTitle("")
+    .navigationBarHidden(true)
+    }
+    }
+   
+    func primaryAPI(){
+       
+//        let param : Parameters  = [
+//              "userName":"9133117471",
+//              "password":"Sree@123"
+//
+//                               ]
+//
+        let param : Parameters  = [
+
+            "userName":mail,
+            "password":password
+        ]
+          
+       
+       let url1 = "https://beta.sreenidhi1sports.com/api/AppUsers/loginWithPassword"
+//
+         print(param)
+    
+        AF.request(url1, method: .post, parameters:param ,encoding: JSONEncoding.default, headers: nil).validate().responseJSON { [self]
+                  response in
+                
+                  switch response.result {
+                  case .success(let value):
+                      do {
+                        var data = value as? [String:Any]
+                        print(data)
+                       
+                        if let token =  data!["token"] as? String {
+                            self.tokenMain = token
+                            
+                            loginAPI()
+                        }
+                      
+                      }
+                      break
+                  case .failure(let error):
+                         
+                      print("failed response ",error)
+      //                print("sendLatLong Err", error.localizedDescription)
+                                    
+                  }
+              }
+    }
+
+    func loginAPI(){
+    
+                let headers:HTTPHeaders = ["Authorization": tokenMain]
+           let url2 = "https://beta.sreenidhi1sports.com/api/AppUsers/fetchUserDetails"
+      
+            AF.request(url2, method: .get, parameters:nil ,encoding: JSONEncoding.default, headers: headers).validate().responseJSON { [self]
+                      response in
+                    
+                      switch response.result {
+                      case .success(let value):
+                          do {
+                        
+                            login1 = true
+                           
+                            
+                            var data = value as? [String:Any]
+                            print(data)
+                          }
+                          break
+                      case .failure(let error):
+                             
+                          print("failed response ",error)
+          //                print("sendLatLong Err", error.localizedDescription)
+                                        
+                      }
+                  }
+          
+        }
+    
+}
+
+struct login_Previews: PreviewProvider {
+    static var previews: some View {
+        login()
+    }
+}
